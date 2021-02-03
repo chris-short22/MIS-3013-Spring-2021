bed0f5083532a935a4e2c6362764257ab1ac4df2
Chris Short <Christopher.Short@ou.edu>
2021-02-02 18:33:31 -0600

    Completed the example over creating variables, getting inputs and creating outputs

--- In_Class_Examples/VariablesInputConversionFormatting_Examples/Program.cs
+++ In_Class_Examples/VariablesInputConversionFormatting_Examples/Program.cs
@@ -6,7 +6,29 @@ namespace VariablesInputConversionFormatting_Examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName, lastName;
+            double gpa;
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; //bump gpa by 0.5 because of the COVID Semester
+
+            string output = "Hello " + firstName + " " + lastName + "! " + " we boosted your gpa to " + gpa.ToString("N3");
+
+            Console.WriteLine(output);
+
+            //string output = "Hello {firstName} {LastName}! we boosted your gpa to {gpa.ToString("N3")";
+            
+        
+
         }
     }
 }
