17c03b8306c3378b3a0eead3b90f90154d7493e4
Chris Short <Christopher.Short@ou.edu>
2021-03-02 17:45:48 -0600

    Capitalizing Taylor Swift and displaying her name vertically

---
+++ StringManupulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//correcting the capitalization of names when the characters are a mix of upper and lower case
+
+namespace StringManupulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string artist = "tAylOr SwIFt";
+            //string newValue = artist.ToLower();
+            artist = artist.ToLower();
+
+            //                              Gives "T"     +      gives "aylor "     +     gives "S"     +     gives "wift"
+            string artistWithProperName = artist.ToUpper()[0]+artist.Substring(1, 6) + artist.ToUpper()[7]+artist.Substring(8);
+            
+
+
+
+           // for (int i = 0; i < artistWithProperName.Length; i++)
+            //{
+              //  Console.WriteLine(artistWithProperName[i]);
+           // }
+
+
+            Console.WriteLine(artistWithProperName); //Console.WriteLine(newValue); either method works
+
+        }
+    }
+}
